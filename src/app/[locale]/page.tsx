cd2bfb083059af9d083d61f809a9704e62fab6bd
khairislama <khairi.slama.1996.br@gmail.com>
2025-03-01 02:07:26 +0100

    feat: :construction: last working basic next intl before moving components to src

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,9 +4,22 @@ import { Link } from "@/src/i18n/navigation";
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <Link href="/about">{t("about")}</Link>
-    </div>
+    <main className="w-full h-screen xl:ml-60">
+      <section className="w-full text-4xl text-center mb-6 p-10">
+        <h1>{t("title")}</h1>
+      </section>
+      <section className="w-full text-4xl text-center mb-6 p-10">
+        <Link href="/about">{t("about")}</Link>
+      </section>
+      <section className="w-full text-4xl text-center mb-6 p-10">
+        <h2>This is a slideshow section</h2>
+      </section>
+      <section className="w-full text-4xl text-center mb-6 p-10">
+        <h2>This is a banner after hero</h2>
+      </section>
+      <section className="w-full text-4xl text-center mb-6 p-10">
+        <h2>This is a grid with filter</h2>
+      </section>
+    </main>
   );
 }
