88995d63467de7175081892c2f83193fb5a3a184
khairislama <khairi.slama.1996.br@gmail.com>
2025-03-06 06:16:12 +0100

    fix: :ambulance: fixed linting

--- src/app/[locale]/maintenance/page.tsx
+++ src/app/[locale]/maintenance/page.tsx
@@ -1,24 +1,26 @@
-import { use } from "react";
-import { setRequestLocale } from "next-intl/server";
-import { useTranslations } from "next-intl";
+// import { use } from "react";
+// import { setRequestLocale } from "next-intl/server";
+// import { useTranslations } from "next-intl";
 import dynamic from "next/dynamic";
 import Slideshow from "@/src/components/slideshow";
 
-type Props = {
-  params: Promise<{ locale: string }>;
-};
+// type Props = {
+//   params: Promise<{ locale: string }>;
+// };
 
+// should I ?
 const Grid = dynamic(() => import("@/src/components/grid"), {
   loading: () => <p>Loading...</p>,
 });
 
-export default function MaintenancePage({ params }: Props) {
-  const { locale } = use(params);
+export default function MaintenancePage() {
+  // { params }: Props
+  // const { locale } = use(params);
 
-  // Enable static rendering
-  setRequestLocale(locale);
+  // // Enable static rendering
+  // setRequestLocale(locale);
 
-  const t = useTranslations("HomePage");
+  // const t = useTranslations("HomePage");
   return (
     <div>
       <section className="w-full text-4xl text-center mb-6 p-10">
