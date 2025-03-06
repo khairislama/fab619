22b38a12b9ecbbbd6f6df8a43f2b0f0efe5f1179
khairislama <khairi.slama.1996.br@gmail.com>
2025-03-06 05:51:39 +0100

    feat: :sparkles: Created grid

--- settings/navigation.ts
+++ settings/navigation.ts
@@ -1,53 +1,86 @@
-import { Github, Goal, Home, Linkedin, Presentation, Send, Twitter, Users } from "lucide-react";
+import {
+  Github,
+  Goal,
+  Home,
+  Linkedin,
+  Presentation,
+  Send,
+  Twitter,
+  Users,
+} from "lucide-react";
 
 // Define allowed URL paths
-export type UrlPath = "/" | "/about" | "/portfolio" | "/services" | "/contact";
+export type UrlPath =
+  | "/"
+  | "/about"
+  | "/portfolio"
+  | "/services"
+  | "/contact"
+  | "/maintenance";
 
-export const NAVIGATION: { title: string; url: UrlPath; icon: React.ComponentType }[]= [
-    {
-      title: "home",
-      url: "/",
-      icon: Home,
-    },
-    {
-      title: "about",
-      url: "/about",
-      icon: Users,
-    },
-    {
-      title: "portfolio",
-      url: "/portfolio",
-      icon: Presentation,
-    },
-    {
-      title: "services",
-      url: "/services",
-      icon: Goal,
-    },
-    {
-      title: "contact",
-      url: "/contact",
-      icon: Send,
-    },
-  ];
+export const NAVIGATION: {
+  title: string;
+  url: UrlPath;
+  icon: React.ComponentType;
+}[] = [
+  {
+    title: "home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "about",
+    url: "/about",
+    icon: Users,
+  },
+  {
+    title: "portfolio",
+    url: "/portfolio",
+    icon: Presentation,
+  },
+  {
+    title: "services",
+    url: "/services",
+    icon: Goal,
+  },
+  {
+    title: "contact",
+    url: "/contact",
+    icon: Send,
+  },
+  {
+    title: "preview",
+    url: "/maintenance",
+    icon: Send,
+  },
+];
 
 export const SITE_LOGO = {
-    large: {
-      url: "/logo/fab-619-logo.svg",
-      alt: "Fab 619 Logo",
-      width: 300,
-      height: 114,
-    },
-    mobile: {
-      url: "/logo/fab-619-mobile-logo.svg",
-      alt: "Fab 619 Mobile Logo",
-      width: 104,
-      height: 150,
-    },
-  };
+  large: {
+    url: "/logo/fab-619-logo.svg",
+    alt: "Fab 619 Logo",
+    width: 300,
+    height: 114,
+  },
+  mobile: {
+    url: "/logo/fab-619-mobile-logo.svg",
+    alt: "Fab 619 Mobile Logo",
+    width: 104,
+    height: 150,
+  },
+};
 
 export const SOCIAL = [
-    { name: "GitHub", href: "https://github.com", icon: Github },
-    { name: "LinkedIn", href: "https://linkedin.com", icon: Linkedin },
-    { name: "Twitter", href: "https://twitter.com", icon: Twitter },
-  ];
+  { name: "Facebook", href: "https://www.facebook.com/fab619", icon: Github },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/fab_619",
+    icon: Twitter,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/company/fab619",
+    icon: Linkedin,
+  },
+  { name: "Github", href: "https://github.com/fab619", icon: Github },
+];
--- src/app/[locale]/maintenance/page.tsx
+++ src/app/[locale]/maintenance/page.tsx
@@ -1,11 +1,15 @@
 import { use } from "react";
 import { setRequestLocale } from "next-intl/server";
-import Maintenance from "@/src/components/Maintenance";
 import { useTranslations } from "next-intl";
+import dynamic from "next/dynamic";
 
 type Props = {
   params: Promise<{ locale: string }>;
 };
+
+const Grid = dynamic(() => import("@/src/components/grid"), {
+  loading: () => <p>Loading...</p>,
+});
 
 export default function MaintenancePage({ params }: Props) {
   const { locale } = use(params);
@@ -19,6 +23,7 @@ export default function MaintenancePage({ params }: Props) {
       <section className="w-full text-4xl text-center mb-6 p-10">
         <h1>hello</h1>
       </section>
+      <Grid />
     </div>
   );
 }
