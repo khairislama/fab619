1dccd4febb4539f17cc740b875dfb68a92250203
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2025-04-30 15:22:15 +0200

    fix: :ambulance: using getTranslation for async functions

--- src/components/projects/Recommendation.tsx
+++ src/components/projects/Recommendation.tsx
@@ -1,15 +1,20 @@
 import ProjectCard from "./ProjectCard";
 import { getProjects } from "@/src/sanity/lib/project/getProjects";
 import { urlFor } from "@/src/sanity/lib/image";
+import { getTranslations } from "next-intl/server";
 
 export default async function Recommendation({ slug }: { slug: string }) {
+  const translation = await getTranslations("portfolio");
+
   const projects = await getProjects();
   const filtered = projects.filter((p) => p.slug !== slug);
   const shuffled = filtered.sort(() => 0.5 - Math.random()).slice(0, 3);
 
   return (
     <div className="mt-16">
-      <h2 className="text-2xl font-bold mb-8">Other Projects</h2>
+      <h2 className="text-2xl font-bold mb-8">
+        {translation("recommendation")}
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {shuffled.map((project) => (
           <ProjectCard
